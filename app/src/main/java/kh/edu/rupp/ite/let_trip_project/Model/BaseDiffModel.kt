5db87d42942681ee796b2d95384b829dbc5180fd
Huoyly <huoylyhort@gmail.com>
2023-12-19 14:36:11 +0700

    delete and change unnecessary file

--- app/src/main/java/kh/edu/rupp/ite/let_trip_project/Model/BaseDiffModel.kt
+++
@@ -1,5 +0,0 @@
-package kh.edu.rupp.ite.let_trip_project.Model
-
-interface BaseDiffModel {
-    fun getItemId(): String
-}
--- app/src/main/java/kh/edu/rupp/ite/let_trip_project/homescreen/dataModel/CategoriesDataModel.kt
+++ app/src/main/java/kh/edu/rupp/ite/let_trip_project/homescreen/dataModel/CategoriesDataModel.kt
@@ -1,20 +1,11 @@
 package kh.edu.rupp.ite.let_trip_project.homescreen.dataModel
 
-import android.os.Parcel
 import android.os.Parcelable
-import com.android.tools.r8.internal.id
 import com.google.gson.annotations.SerializedName
-import kh.edu.rupp.ite.let_trip_project.Model.BaseDiffModel
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class CategoriesDataModel(
     @SerializedName("id") val id: String
 
-    ) : BaseDiffModel, Parcelable {
-
-
-    override fun getItemId(): String = id
-
-
-}
+    ) : Parcelable
